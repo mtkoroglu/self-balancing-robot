ddaa9d284b3c60bc95c72f7f4c34a00f11552028
gumushane-eem-eesec-440 <82487402+gumushane-eem-eesec-440@users.noreply.github.com>
2021-12-31 15:10:48 +0300

    Update robot.ino

--- code/robot.ino
+++ code/robot.ino
@@ -1,14 +1,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////
-//Terms of use
+// Terms of use
 ///////////////////////////////////////////////////////////////////////////////////////
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 ///////////////////////////////////////////////////////////////////////////////////////
+
+/*
+  Written by: Joop Brokking
+  Modified by: M. Taha Koroglu
+  -------------------------------- ROBOT KODU -----------------------------------------
+*/
 
 #include <Wire.h> //Include the Wire.h library so we can communicate with the gyro
 #include <SPI.h>
